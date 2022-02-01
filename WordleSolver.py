8a6edca8a1656f76a9260813899e03f23044dbee
Matthew Dulchinos <dulchinm@union.edu>
2022-02-01 18:19:59 -0500

    Added in list of letters who are known not to be in the word. Also removed case sentistivity

--- WordleSolver.py
+++ WordleSolver.py
@@ -2,15 +2,20 @@
 #Easy program to solve through Wordle games.
 
 #Replace lists with currently known letters
-KNOWN_LETTERS = ["a","b","s"]
+KNOWN_LETTERS = ["h"]
 #Each pos should match with the known position of the letter above, or -1 if it is unknown
-KNOWN_POS = [1, -1, 0]
+KNOWN_POS = [1]
+
+#Letters we know not in the word
+INVALID_LETTERS = ["c","A","i","r"]
 
 def main():
     #Below line only needs to be uncommented if you lost the trimmed list
     #generateInitList()
 
     curList = loadInitList()
+    for letter in INVALID_LETTERS:
+        curList = checkForInvalidLetter(letter, curList)
     for x in range(len(KNOWN_LETTERS)):
         letter = KNOWN_LETTERS[x]
         pos = KNOWN_POS[x]
@@ -40,11 +45,19 @@ def loadInitList():
     
     return curList
 
+def checkForInvalidLetter(letter, curList):
+    shortenedList = []
+    for word in curList:
+        if(letter.lower() not in word):
+            shortenedList.append(word)
+    return shortenedList
+                
+
 def checkForLetterPos(letter, pos, curList):
     """Eliminate words that don't have the letter at the specific pos in the word"""
     shortenedList = []
     for word in curList:
-        if(word[pos] == letter):
+        if(word[pos] == letter.lower()):
             shortenedList.append(word)
     return shortenedList
 
@@ -52,7 +65,7 @@ def checkForLetterNoPos(letter, curList):
     """Eliminate words that don't have the letter in the word"""
     shortenedList = []
     for word in curList:
-        if(letter in word):
+        if(letter.lower() in word):
             shortenedList.append(word)
     return shortenedList
 
